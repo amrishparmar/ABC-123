6a9831c06732b0cd321d5e46a15d08d328e63d22
aborext <bruce358241@hotmail.com>
2014-12-03 09:44:23 +0000

    Update num_scripts.js

--- js/num_scripts.js
+++ js/num_scripts.js
@@ -4,26 +4,26 @@ var Number = function(numeral, word, sound) {
 	this.sound = sound;
 }
 
-var one = new Number(1, 'One', new Audio('sound/num/one.mp3'));
-var two = new Number( 2, 'Two', new Audio('sound/num/one.mp3'));
-var three = new Number( 3, 'Three', new Audio('sound/num/one.mp3'));
-var four = new Number( 4, 'Four', new Audio('sound/num/one.mp3'));
-var five = new Number( 5, 'Five', new Audio('sound/num/one.mp3'));
-var six = new Number( 6, 'Six', new Audio('sound/num/one.mp3'));
-var seven = new Number( 7, 'Seven', new Audio('sound/num/one.mp3'));
-var eight = new Number( 8, 'Eight', new Audio('sound/num/one.mp3'));
-var nine = new Number( 9, 'Nine', new Audio('sound/num/one.mp3'));
-var ten = new Number( 10, 'Ten', new Audio('sound/num/one.mp3'));
-var eleven = new Number( 11, 'Eleven', new Audio('sound/num/one.mp3'));
-var twelve = new Number( 12, 'Twelve', new Audio('sound/num/one.mp3'));
-var thirteen = new Number( 13, 'Thirteen', new Audio('sound/num/one.mp3'));
-var fourteen = new Number( 14, 'Fourteen', new Audio('sound/num/one.mp3'));
-var fifteen = new Number( 15, 'Fifteen', new Audio('sound/num/one.mp3'));
-var sixteen = new Number( 16, 'Sixteen', new Audio('sound/num/one.mp3'));
-var seventeen = new Number( 17, 'Seventeen', new Audio('sound/num/one.mp3'));
-var eighteen = new Number( 18, 'Eighteen', new Audio('sound/num/one.mp3'));
-var nineteen = new Number( 19, 'Nineteen', new Audio('sound/num/one.mp3'));
-var twenty = new Number( 20, 'Twenty', new Audio('sound/num/one.mp3'));
+var one = new Number(1, 'One', new Audio('sound/num/One.mp3'));
+var two = new Number( 2, 'Two', new Audio('sound/num/Two.mp3'));
+var three = new Number( 3, 'Three', new Audio('sound/num/Three.mp3'));
+var four = new Number( 4, 'Four', new Audio('sound/num/Four.mp3'));
+var five = new Number( 5, 'Five', new Audio('sound/num/Five.mp3'));
+var six = new Number( 6, 'Six', new Audio('sound/num/Six.mp3'));
+var seven = new Number( 7, 'Seven', new Audio('sound/num/Seven.mp3'));
+var eight = new Number( 8, 'Eight', new Audio('sound/num/Eight.mp3'));
+var nine = new Number( 9, 'Nine', new Audio('sound/num/Nine.mp3'));
+var ten = new Number( 10, 'Ten', new Audio('sound/num/Ten.mp3'));
+var eleven = new Number( 11, 'Eleven', new Audio('sound/num/Eleven.mp3'));
+var twelve = new Number( 12, 'Twelve', new Audio('sound/num/Twelve.mp3'));
+var thirteen = new Number( 13, 'Thirteen', new Audio('sound/num/Thirteen.mp3'));
+var fourteen = new Number( 14, 'Fourteen', new Audio('sound/num/Fourteen.mp3'));
+var fifteen = new Number( 15, 'Fifteen', new Audio('sound/num/Fifteen.mp3'));
+var sixteen = new Number( 16, 'Sixteen', new Audio('sound/num/Sixteen.mp3'));
+var seventeen = new Number( 17, 'Seventeen', new Audio('sound/num/Seventeen.mp3'));
+var eighteen = new Number( 18, 'Eighteen', new Audio('sound/num/Eighteen.mp3'));
+var nineteen = new Number( 19, 'Nineteen', new Audio('sound/num/Nineteen.mp3'));
+var twenty = new Number( 20, 'Twenty', new Audio('sound/num/Twenty.mp3'));
 
 var numbers = [one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eighteen,nineteen,twenty];
 
